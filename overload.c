df04ecf756fdd94196143a35d81415da1ce02c07
lyb19930105 <357089499@qq.com>
2016-06-07 19:48:54 +0800

    git add -A

--- overload.c
+++ overload.c
@@ -1,21 +1,19 @@
  ///
- /// @file    default.c
+ /// @file    overload.c
  /// @author  lemon(haohb13@gmail.com)
  /// @date    2016-06-07 14:53:24
  ///
 
 #include <stdio.h>
-#include <iostream>
-using std::cout;
-using std::endl;
+
+//C语言不支持重载
 
 int add(int a, int b)
 {
 	return a + b;
 }	
  
-//默认参数或者缺省参数的赋值只能从右到左进行
-int add(int a, double b, int c = 0)
+int add(int a, int b, int c)
 {
 	return a + b + c;
 }
@@ -24,7 +22,6 @@ int main(void)
 {
 	int a = 3;
 	int b = 4;
-	cout << add(a,b) << endl;
 	printf("a + b = %d\n", add(a, b));
 	return 0;
 }
